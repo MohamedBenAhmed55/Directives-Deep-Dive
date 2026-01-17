113b65c27e3c0dccd0ae4d36420cf446127eedee
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-01-17 22:52:22 +0100

    Building a Custom Structural Directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,5 +1,5 @@
 import { Permission } from './auth.model';
-import { Directive, effect, inject, input } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Directive({
@@ -8,16 +8,20 @@ import { AuthService } from './auth.service';
 })
 export class AuthDirective {
   userType = input.required<Permission>({alias:'appAuth'});
-  authService = inject(AuthService)
+  private authService = inject(AuthService);
+  // access to the content of a template
+  private templateRef = inject(TemplateRef);
+  // access to the place in the DOM where the directive is being used
+  private viewContainerRef = inject(ViewContainerRef);
 
   
   constructor() {
     // using effect to react to signal changes as it creates subscriptinos to them 
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-        console.log('SHOW ELEMENT');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('DO NOT SHOW ELEMENT')
+        this.viewContainerRef.clear();
       }
     })
   }
